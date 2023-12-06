c59e3f731790a6d91a0d9b3604094603675ec137
Matthew Byrne <matthewb@Matthews-MacBook-Pro.local>
2023-12-06 19:32:17 +0000

    New Detail View with Urls

--- Models/Character.cs
+++ Models/Character.cs
@@ -29,4 +29,40 @@ namespace GOTCharacterApp.Models
         public List<string> TvSeries { get; set; }
         public List<string> PlayedBy { get; set; }
     }
+
+        public class Book
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Isbn { get; set; }
+        public List<string> Authors { get; set; }
+        public int NumberOfPages { get; set; }
+        public string Publisher { get; set; }
+        public string Country { get; set; }
+        public string MediaType { get; set; }
+        public DateTime Released { get; set; }
+        public List<string> Characters { get; set; }
+        public List<string> PovCharacters { get; set; }
+    }
+
+        public class House
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string CoatOfArms { get; set; }
+        public string Words { get; set; }
+        public List<string> Titles { get; set; }
+        public List<string> Seats { get; set; }
+        public string CurrentLord { get; set; }
+        public string Heir { get; set; }
+        public string Overlord { get; set; }
+        public string Founded { get; set; }
+        public string Founder { get; set; }
+        public string DiedOut { get; set; }
+        public List<string> AncestralWeapons { get; set; }
+        public List<string> CadetBranches { get; set; }
+        public List<string> SwornMembers { get; set; }
+    }
+    
 }
--- Services/GOTService.cs
+++ Services/GOTService.cs
@@ -2,6 +2,8 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using GOTCharacterApp.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GOTCharacterApp.Services
 {
@@ -25,11 +27,44 @@ namespace GOTCharacterApp.Services
             return await _httpClient.GetFromJsonAsync<IceAndFireCharacter>($"https://anapioficeandfire.com/api/Characters/{id}");
         }
 
-         public async Task<IceAndFireCharacter> GetIceAndFireCharacterByNameAsync(string name)
+        public async Task<IceAndFireCharacter> GetIceAndFireCharacterByNameAsync(string name)
         {
             var characters = await _httpClient.GetFromJsonAsync<List<IceAndFireCharacter>>($"https://anapioficeandfire.com/api/Characters?name=" + Uri.EscapeDataString(name));
             return characters.FirstOrDefault();
         }
+
+        // Method to get a house by URL
+        public async Task<House> GetHouseByUrlAsync(string url)
+        {
+            return await _httpClient.GetFromJsonAsync<House>(url);
+        }
+
+        // Method to get a book by URL
+        public async Task<Book> GetBookByUrlAsync(string url)
+        {
+            return await _httpClient.GetFromJsonAsync<Book>(url);
+        }
+
+        // Method to get a character by URL
+        public async Task<Character> GetCharacterByUrlAsync(string url)
+        {
+            return await _httpClient.GetFromJsonAsync<Character>(url);
+        }
+
+        public async Task<string> GetCharacterNameByUrlAsync(string url)
+        {
+            var character = await _httpClient.GetFromJsonAsync<Character>(url);
+            return character != null ? character.FullName : null;
+        }
+
+        // In GOTService.cs
+        public async Task<string> GetHouseNameByUrlAsync(string url)
+        {
+            var house = await _httpClient.GetFromJsonAsync<House>(url);
+            return house != null ? house.Name : null;
+        }
+
+        
 
     }
 }
